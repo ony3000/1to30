96b3dc732d7f8bec2ed8f9c0ff6393c2324f4f1f
Hyeonjong <nianelo4@gmail.com>
2023-04-02 16:03:30 +0900

    Update BoxLink.tsx

--- src/components/BoxLink.tsx
+++ src/components/BoxLink.tsx
@@ -1,7 +1,4 @@
-import { Inter } from 'next/font/google';
 import classNames from 'classnames';
-
-const inter = Inter({ subsets: ['latin'] });
 
 type BoxLinkProps = {
   href: string;
@@ -14,35 +11,27 @@ export default function BoxLink({ href, title, description }: BoxLinkProps) {
     <a
       href={href}
       className={classNames(
-        'group rounded-xl border border-solid py-4 px-10 transition-colors duration-200 ease-ease',
+        'group space-y-2 rounded-xl border p-4 transition-colors duration-200',
         'border-zinc-500/0 bg-gray-400/0',
         'dark:border-stone-300/0 dark:bg-stone-500/0',
-        'tablet:px-[19.2px]',
         'non-touch-device:hover:border-zinc-500/[.15] non-touch-device:hover:bg-gray-400/10',
         'dark:non-touch-device:hover:border-stone-300/[.15] dark:non-touch-device:hover:bg-stone-500/10',
       )}
       target="_blank"
       rel="noopener noreferrer"
     >
-      <h2 className={classNames(inter.className, 'mb-2 font-semibold tablet:mb-[11.2px]')}>
+      <h2 className="font-semibold">
         {title}{' '}
         <span
           className={classNames(
-            'inline-block transition-transform duration-200 ease-ease',
+            'inline-block transition-transform duration-200',
             'motion-safe:non-touch-device:group-hover:translate-x-1',
           )}
         >
-          -&gt;
+          &rarr;
         </span>
       </h2>
-      <p
-        className={classNames(
-          inter.className,
-          'm-0 max-w-[30ch] text-[14.4px] leading-normal opacity-60',
-        )}
-      >
-        {description}
-      </p>
+      <p className="text-[14px] opacity-60">{description}</p>
     </a>
   );
 }
