a6c42e7ee9d278be773993df3ae7b39547734036
Hyeonjong <nianelo4@gmail.com>
2023-04-12 00:00:55 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const withPWA = require('next-pwa')({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
 };
 
 module.exports = withPWA(nextConfig);
