d246c213abd2c93da6cd13e42020b74b3b3b440d
Hyeonjong <nianelo4@gmail.com>
2023-04-12 23:00:18 +0900

    Remove unnecessary code generated by Create Next App

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,1 @@
 export { default as AppMain } from './AppMain';
-export { default as BoxLink } from './BoxLink';
-export { default as Callout } from './Callout';
-export { default as NextByVercel } from './NextByVercel';
