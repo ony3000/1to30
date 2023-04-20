55034e08c2f5b8a1dbfd18d75ddc4344f608502b
Hyeonjong <nianelo4@gmail.com>
2023-04-21 00:24:10 +0900

    Update classnames.test.ts

--- tests/adaptors/classnames.test.ts
+++ tests/adaptors/classnames.test.ts
@@ -10,4 +10,14 @@ describe('classNames adaptor', () => {
       ),
     ).toBe('first second third');
   });
+
+  it('template literal로 작성한 조건부 multi-line class name을 single-line class name으로 변환한다.', () => {
+    expect(
+      classNames({
+        [`first
+        second
+        third`]: true,
+      }),
+    ).toBe('first second third');
+  });
 });
